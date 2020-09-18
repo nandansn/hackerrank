8da708c00b025642bd0c506aa644db31fd6039aa
nanda.r <geeknanda13@gmail.com>
2020-09-18 20:53:20 +0530

    updated

--- src/com/practice/MinimumBribes.java
+++ src/com/practice/MinimumBribes.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MinimumBribes {
 	
 	public static void main(String[] args) {
-		minimumBribes(new int[] {2,1,5,3,4});
+		minimumBribes(new int[] {1, 2, 5, 3, 7, 8, 6, 4});
 	}
 	
 	static void minimumBribes(int[] q) {
@@ -13,36 +13,51 @@ public class MinimumBribes {
 		int reorderBribeCount = 0;
 		boolean printFlag = true;
 		
-		for (int i=0; i<q.length; i++) {
-			
-			int originalPosition = q[i] - 1;
-			
-			int bribedPosition = i;
-			
-			int diffInPosition = originalPosition - bribedPosition;
-			
-			if (diffInPosition == 0) {
+		int[] arrayNonBribed = Arrays.copyOf(q,q.length);
+		
+		Arrays.parallelSort(arrayNonBribed);
+		
+		for (int i =0; i<q.length; i++) {
+			if (q[i] == arrayNonBribed[i]) {
 				continue;
-			}
-			
-			if (diffInPosition >= -2 && diffInPosition <= 2) {
-				reorderBribeCount = Math.abs(diffInPosition) + reorderBribeCount;
-				//i = originalPosition;
 			} else {
-				System.out.println("Too chaotic");
-				printFlag = false;
-				break;
-			}
-			
+				int originalPosOfBribedElement = q[i] - 1;
+				int currentPosOfBribedElement = i;
+				
+				int bribeCountLimitPerPerson =0;
+				if ( currentPosOfBribedElement < originalPosOfBribedElement) {
+					for (int j = originalPosOfBribedElement; j > currentPosOfBribedElement; j--) {
+							int temp = arrayNonBribed[originalPosOfBribedElement];
+							arrayNonBribed[originalPosOfBribedElement] = arrayNonBribed[originalPosOfBribedElement - 1];
+							arrayNonBribed[originalPosOfBribedElement - 1] = temp;
+							originalPosOfBribedElement = originalPosOfBribedElement - 1;
+							++bribeCountLimitPerPerson;
+							reorderBribeCount = 1 + reorderBribeCount;
+					}
+				}
+				
+				if (bribeCountLimitPerPerson > 2) {
+					printFlag = false;
+					break;
+				}
 		}
 		
-		if (printFlag) {
-			System.out.println(reorderBribeCount);
-		}
+		
+		
+		
+		
+		
+		
 		
 		
 
     }
+		
+		if (printFlag) {
+			System.out.println(reorderBribeCount);
+		} else {
+			System.out.println("Too chaotic");
+		}
 
-
-}
+	}
+	}
